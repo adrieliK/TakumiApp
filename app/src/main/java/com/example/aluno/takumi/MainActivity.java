9bc6216ef9488b67ce7bde8d3746184f4c09375c
adrielik <adrielikethin.dossantos@hotmail.com>
2019-08-12 11:32:27 -0300

    mapas

--- app/src/main/java/com/example/aluno/takumi/MainActivity.java
+++ app/src/main/java/com/example/aluno/takumi/MainActivity.java
@@ -1,44 +1,64 @@
 package com.example.aluno.takumi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private Button bloc;
+    private Button bface;
+    private Button bped;
+    private Button bcard;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.elementos);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bloc = (Button) findViewById(R.id.bloc);
+        bface = (Button) findViewById(R.id.bface);
+        bped = (Button) findViewById(R.id.bped);
+        bcard = (Button) findViewById(R.id.bcard;
+
+        bloc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                // Comando para chamar outra tela
+                // o new Intent precisa de dois parâmetros: a tela atual.this e a tela que será chamada.class
+                startActivity( new Intent(MainActivity.this, Localizacao.class) );
+            }
+        });
+
+
+        bped.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Pedido.class));
+            }
+        });
+
+        bcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                startActivity(new Intent(MainActivity.this, Cardapio.class));
+            }
+        });
+
+
+
     }
 
 }
